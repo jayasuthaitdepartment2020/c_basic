92c95f9b2b339884f15c6efdc440c3ca3043fd51
princess sutha <81137735+jayasuthaitdepartment2020@users.noreply.github.com>
2022-04-26 17:39:15 +0530

    this is for just check to local repository

--- c_hello.c
+++ c_hello.c
@@ -2,6 +2,7 @@
 int main()
 {
     printf("hello my c programming world");
+    printf("i can change the main repository");
     printf("c program run in terminal - gcc c_hello.c -o c_hello");
     return 0;
 }

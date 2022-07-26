49bd0af24dc74b455b68db496a5e2bc53eebeeaa
princess sutha <jayasuthaitdepartment2020@gmail.com>
2022-07-26 10:34:32 +0530

    mic test one two three

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("enter the first value :");
+    scanf("%d", &a);
+    printf("enter the second value :");
+    scanf("%d", &b);
+    c = a + b;
+    printf("the add value id %d", c);
+}
